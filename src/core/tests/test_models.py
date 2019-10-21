4e5aa3919dc3b26e58cd3f1797a53f115b0514fe
eyakub <eyakubsorkar@gmail.com>
2019-10-22 03:28:36 +0600

    added core app and created user model to custom and changed username as email

---
+++ src/core/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """ Test creatin g a new user with an email is successful """
+        email = 'test@eyakub.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))

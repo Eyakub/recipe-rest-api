8855a0cd27d605f1f55047688afa3bb87942c457
eyakubsorkar <eyakubsorkar@gmail.com>
2019-10-24 19:46:11 +0600

    11-2 done on user test case

---
+++ src/user/tests/test_user_api.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """ test the users api (public) """
+
+    def setup(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """ test creating user with valid payload is successful"""
+        payload = {
+            'email': 'test@eyakub.com',
+            'password': 'testpass',
+            'name': 'Test Eyakub'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """ Test creating a user that already exists fail"""
+        payload = {'email': 'test@eyakub.com', 'password': 'testpass'}
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_password_too_short(self):
+        """ Test that the password must be more than five character"""
+        payload = {
+            'email': 'test@eyakub.com',
+            'password': 'pw',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)

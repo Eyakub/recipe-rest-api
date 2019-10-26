adb72c7f9356a416956cb98998228cc65d588ce1
eyakubsorkar <eyakubsorkar@gmail.com>
2019-10-26 17:30:49 +0600

    manage user endpoint api and 11 done

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -14,12 +14,23 @@ class UserSerializer(serializers.ModelSerializer):
         """ create a new user with encrypted password and return it """
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """ Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        # super is going to call the ModelSerializer build in UPDATE method
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """ serializer for the user authentication object """
     email = serializers.CharField()
     password = serializers.CharField(
-        style = {'input_type': 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False
     )
 

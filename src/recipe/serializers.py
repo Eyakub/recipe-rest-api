325e37eae5e8315cb1760f8ef2b956dcb943e236
eyakubsorkar <eyakubsorkar@gmail.com>
2019-10-30 13:39:15 +0600

    1. ingredient serializer and viewset for web api view

--- src/recipe/serializers.py
+++ src/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -8,5 +8,17 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ("id", "name")
+        read_only_fields = ("id",)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """ serializer for ingredient objects """
+
+    class Meta:
+        model = Ingredient
+        fields = (
+            "id",
+            "name",
+        )
         read_only_fields = ("id",)
 
--- src/recipe/views.py
+++ src/recipe/views.py
@@ -3,7 +3,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from .serializers import *
 
 
@@ -24,3 +24,17 @@ class TagViewSet(
     def perform_create(self, serializer):
         """ Create a new tag """
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """ Manage ingredient in the database """
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        """ return objects for the current authenticated user """
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
